dda3462916bc3a71ec86cc0e58366968cd6ae20b
HectorSanchezLuque <92037326+HectorSanchezLuque@users.noreply.github.com>
2022-12-08 16:10:35 +0100

    Add files via upload

---
+++ AE03_Mongo/src/es/florida/AE03Mongo/Vista.java
@@ -0,0 +1,10 @@
+package es.florida.AE03Mongo;
+
+public class Vista {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AE03_Mongo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author RxRos
+ *
+ */
+module AE03_Mongo {
+}
