97b36991aad88455483e934ca12633da754e0c30
HectorSanchezLuque <hesalu@floridauniversitaria.es>
2022-12-14 20:23:48 +0100

    MongoCursor

--- AE03_Mongo/src/es/florida/AE03Mongo/Model.java
+++ AE03_Mongo/src/es/florida/AE03Mongo/Model.java
@@ -12,61 +12,82 @@ import org.bson.Document;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
-
-
+import static com.mongodb.client.model.Filters.eq;
 
 public class Model {
-	
+
 	String ip;
 	String port;
 	String db;
 	String llibres;
-	String users ;
-	
-	
+	String users;
+
 	public Model() {
 		JSONParser jsonParser = new JSONParser();
 		try {
 			FileReader reader = new FileReader("./src/connect.json");
-			
-			 Object obj = jsonParser.parse(reader);
-			 
-			 JSONObject jo = (JSONObject) obj;
-			 
-			 this.ip = (String) jo.get("ip");
-			 this.port= (String) jo.get("port");
-			 this.db = (String) jo.get("db");
-			 this.llibres= (String) jo.get("llibres");
-			this.users=(String) jo.get("users");
-			
+
+			Object obj = jsonParser.parse(reader);
+
+			JSONObject jo = (JSONObject) obj;
+
+			this.ip = (String) jo.get("ip");
+			this.port = (String) jo.get("port");
+			this.db = (String) jo.get("db");
+			this.llibres = (String) jo.get("llibres");
+			this.users = (String) jo.get("users");
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
+
 	public String MongoConversor(String contr) throws NoSuchAlgorithmException {
 
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        BigInteger number = new BigInteger(1, md.digest(contr.getBytes(StandardCharsets.UTF_8)));
-        StringBuilder hexString = new StringBuilder(number.toString(16));
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		BigInteger number = new BigInteger(1, md.digest(contr.getBytes(StandardCharsets.UTF_8)));
+		StringBuilder hexString = new StringBuilder(number.toString(16));
 
-        while (hexString.length() < 64)
-        {
-            hexString.insert(0, '0');
-        }
- 
-        return hexString.toString();
-		
+		while (hexString.length() < 64) {
+			hexString.insert(0, '0');
+		}
+
+		return hexString.toString();
+
 	}
+
 
 	public Boolean MongoCompUser(String usuari, String contr) {
+
+		Boolean comprobar = false;
+
+		MongoClient mongoClient = new MongoClient(this.ip, Integer.parseInt(this.port));
+		MongoDatabase database = mongoClient.getDatabase(this.db);
+		MongoCollection<Document> coleccion = database.getCollection(this.users);
+		// CRUD operations
+		mongoClient.close();
 		
-		return true;
+		BasicDBObject criteri = new BasicDBObject();
+		criteri.append("usuari", usuari);
+		criteri.append("contrasenya", contr);
+		
+		MongoCursor<Document> cursor = (MongoCursor<Document>) coleccion.find(criteri);
+		while (cursor.hasNext()) {
+			if (cursor.next().toString().equals(usuari)) {
+				comprobar = true;
+			}
+		}
+
+		return comprobar;
 	}
-	
-	
+
 }
