f8be3cc36f5db1d39ed782687bc5e587afe92b9e
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-12-14 19:46:03 +0100

    me lo bajo

--- AE03_Mongo/src/es/florida/AE03Mongo/Model.java
+++ AE03_Mongo/src/es/florida/AE03Mongo/Model.java
@@ -4,8 +4,11 @@ import java.io.FileNotFoundException;
 
 import java.io.FileReader;
 
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
+
+
 public class Model {
 	
 	
@@ -18,13 +21,13 @@ public class Model {
 	public Model() {
 		JSONParser jsonParser = new JSONParser();
 		try {
-			FileReader reader = new FileReader("./connect.json");
+			FileReader reader = new FileReader("./src/connect.json");
 			
 			 Object obj = jsonParser.parse(reader);
 			 
 			 JSONObject jo = (JSONObject) obj;
 			 
-			 this.ip = (String) jo.get("id");
+			 this.ip = (String) jo.get("ip");
 			 this.port= (String) jo.get("port");
 			 this.db = (String) jo.get("db");
 			 this.coleccio= (String) jo.get("coleccio");
