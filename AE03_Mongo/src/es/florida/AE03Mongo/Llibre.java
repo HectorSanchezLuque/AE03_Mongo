2c6e34c071c39b54f41036a3d0af35b3fe7f9ce9
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-12-16 18:53:59 +0100

    hey

--- AE03_Mongo/src/es/florida/AE03Mongo/Llibre.java
+++ AE03_Mongo/src/es/florida/AE03Mongo/Llibre.java
@@ -1,16 +1,47 @@
 package es.florida.AE03Mongo;
 
+import java.awt.Image;
+
 public class Llibre {
-	
+
 	int id;
-	
+
 	String titol;
-	
+
 	int pagines;
-	
+
 	String editorial;
-	
-	
+
+	Image imatge;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getTitol() {
+		return titol;
+	}
+
+	public int getPagines() {
+		return pagines;
+	}
+
+	public String getEditorial() {
+		return editorial;
+	}
+
+	public Image getImatge() {
+		return imatge;
+	}
+
+	public Llibre(int id, String titol, int pagines, String editorial, Image imatge) {
+		super();
+		this.id = id;
+		this.titol = titol;
+		this.pagines = pagines;
+		this.editorial = editorial;
+		this.imatge = imatge;
+	}
 	
 	
 
